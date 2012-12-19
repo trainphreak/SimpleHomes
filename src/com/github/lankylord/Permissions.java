8c6933b4178d5cc18207d89dfbc0b9b83d47d66c
Scott Kendall <lankylord@myself.com>
2012-12-19 16:46:32 +0000

    Attempted to rewrite the way permissions are handled

--- src/com/github/lankylord/Permissions.java
+++
@@ -1,8 +0,0 @@
-package com.github.lankylord;
-
-import org.bukkit.permissions.Permission;
-
-public class Permissions {
-    public Permission canUseHomes = new Permission("simplehomes.home");
-    public Permission canTeleportOther = new Permission("simplehomes.otherhome");
-}
