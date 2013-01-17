27225b768c1c750fb58e3213d713ba2498446a0a
LankyLord <>
2013-01-17 21:28:54 +0000

    Added multi-home support

--- src/com/github/lankylord/SimpleHomes.java
+++ src/com/github/lankylord/SimpleHomes.java
@@ -1,7 +1,6 @@
 package com.github.lankylord;
 
 import java.util.logging.Logger;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
@@ -32,16 +31,30 @@ public class SimpleHomes extends JavaPlugin{
             case "sethome":
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
-                    if (sender.hasPermission("simplehomes.homes")) {
-                        getConfig().set(player.getName() + ".x",
-                                player.getLocation().getBlockX());
-                        getConfig().set(player.getName() + ".y",
-                                player.getLocation().getBlockY());
-                        getConfig().set(player.getName() + ".z",
-                                player.getLocation().getBlockZ());
-                        saveConfig();
-                        sender.sendMessage(ChatColor.YELLOW + "Home set.");
-                        break;
+                    if(args.length == 0) {
+                        if (sender.hasPermission("simplehomes.homes")) {
+                            getConfig().set(player.getName() + ".x",
+                                    player.getLocation().getBlockX());
+                            getConfig().set(player.getName() + ".y",
+                                    player.getLocation().getBlockY());
+                            getConfig().set(player.getName() + ".z",
+                                    player.getLocation().getBlockZ());
+                            saveConfig();
+                            sender.sendMessage(ChatColor.YELLOW + "Home set.");
+                            break;
+                        }
+                    } else if(args.length == 1) {
+                        if (sender.hasPermission("simplehomes.homes")) {
+                            getConfig().set(player.getName() + "." + args[0] + ".x",
+                                    player.getLocation().getBlockX());
+                            getConfig().set(player.getName() + "." + args[0] + ".y",
+                                    player.getLocation().getBlockY());
+                            getConfig().set(player.getName() + "." + args[0] + ".z",
+                                    player.getLocation().getBlockZ());
+                            saveConfig();
+                            sender.sendMessage(ChatColor.YELLOW + "Home set.");
+                            break;
+                        }
                     }
                 }
             case "home":
@@ -56,17 +69,37 @@ public class SimpleHomes extends JavaPlugin{
                             sender.sendMessage(ChatColor.YELLOW + "Teleported.");
                         }
                     } else if(args.length == 1) {
+                        if (sender.hasPermission("simplehomes.multihomes")) {
+                            int x = getConfig().getInt(player.getName() + "." + args[0] + ".x"), y = getConfig()
+                                    .getInt(player.getName() + "." + args[0] + ".y"), z = getConfig()
+                                    .getInt(player.getName() + "." + args[0] + ".z");
+                            player.teleport(new Location(player.getWorld(), x, y, z));
+                            sender.sendMessage(ChatColor.YELLOW + "Teleported.");
+                            break;
+                        }
+                    }
+                }
+            case "otherhome":
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    if(args.length == 1) {
                         if (sender.hasPermission("simplehomes.otherhomes")) {
                             int x = getConfig().getInt(args[0] + ".x"), y = getConfig()
                                     .getInt(args[0] + ".y"), z = getConfig()
                                     .getInt(args[0] + ".z");
                             player.teleport(new Location(player.getWorld(), x, y, z));
                             sender.sendMessage(ChatColor.YELLOW + "Teleported to " + args[0] + "'s home.");
-                            break;
+                        }
+                    } else if(args.length == 2) {
+                        if (sender.hasPermission("simplehomes.otherhomes")) {
+                            int x = getConfig().getInt(args[0] + "." + args[1] + ".x"), y = getConfig()
+                                    .getInt(args[0] + "." + args[1] + ".y"), z = getConfig()
+                                    .getInt(args[0] + "." + args[1] + ".z");
+                            player.teleport(new Location(player.getWorld(), x, y, z));
+                            sender.sendMessage(ChatColor.YELLOW + "Teleported to " + args[0] + "'s home.");
                         }
                     }
                 }
-        }
-        return false;
+        } return false;
     }
 }
