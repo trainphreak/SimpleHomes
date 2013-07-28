cfd88f98274b37ed3f24dd03092b3898f1e227b3
Chris Darnell <chris@cedeel.com>
2013-07-28 18:23:49 -0400

    Fixed command handling.

--- src/main/java/net/lankylord/simplehomes/SimpleHomes.java
+++ src/main/java/net/lankylord/simplehomes/SimpleHomes.java
@@ -5,15 +5,21 @@ import net.lankylord.simplehomes.commands.*;
 import net.lankylord.simplehomes.managers.HomeFileManager;
 import net.lankylord.simplehomes.util.PermissionsModule;
 import net.lankylord.simplehomes.util.Updater;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.MetricsLite;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 
 public class SimpleHomes extends JavaPlugin {
 
     private final HomeFileManager homeFileManager;
+    private CommandHandler commandHandler;
 
     public SimpleHomes() {
         this.homeFileManager = new HomeFileManager(this);
@@ -38,6 +44,14 @@ public class SimpleHomes extends JavaPlugin {
         getLogger().log(Level.INFO, "SimpleHomes Disabled!");
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        List<String> allArgs = new ArrayList<>();
+        allArgs.addAll(Arrays.asList(args));
+        allArgs.add(0, label);
+        return commandHandler.locateAndRunCommand(sender, allArgs);
+    }
+
     private void loadMetrics() {
         try {
             MetricsLite metrics = new MetricsLite(this);
@@ -54,7 +68,7 @@ public class SimpleHomes extends JavaPlugin {
 
     private void loadCommands() {
         PermissionsModule pm = new PermissionsModule();
-        CommandHandler commandHandler = new CommandHandler(this, pm);
+        commandHandler = new CommandHandler(this, pm);
         commandHandler.registerCommand(new DeleteHomeCommand(this));
         commandHandler.registerCommand(new HomeCommand(this));
         commandHandler.registerCommand(new OtherHomeCommand(this));
